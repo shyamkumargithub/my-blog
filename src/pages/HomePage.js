0aed9bfc616161ae8fbcf6dc14a9ca8634728b64
shyam <shyamlaishram79@gmail.com>
2023-12-21 12:49:55 +0530

    add form page in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,45 +6,147 @@ const HomePage = () => {
     document.title = 'Home';
   }, []);
   return (
-    <>
-      <h1>Hello, welcome to my blog!</h1>
-
-      <p>
-        Welcome to my blog! Proin congue ligula id risus posuere, vel eleifend
-        ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt
-        egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In
-        tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non
-        fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus
-        euismod.
-      </p>
-      <p>
-        Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices
-        dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu.
-        Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas
-        viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo
-        luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue
-        tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam.
-        Vivamus accumsan dui in facilisis aliquet.,
-      </p>
-      <div className="App">
-        <h1>Test</h1>
-        <div
-        id="app"
-          data-symbol="AMZN"
-          className="nicoraynaud-finance-widget"
-          data-token="asdasdm#234vt5t3t5asdal"
-        ></div>
+    <div className="max-w-full rounded overflow-hidden shadow-lg">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
       </div>
-      <p>
-        Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit
-        tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan
-        nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec
-        finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi
-        vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac
-        vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan
-        dui in facilisis aliquet.,
-      </p>
-    </>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
+
+      <form className="w-full max-w-lg">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-first-name"
+            >
+              First Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+            />
+            <p className="text-red-500 text-xs italic">
+              Please fill out this field.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-last-name"
+            >
+              Last Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Doe"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              Password
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-gray-600 text-xs italic">
+              Make it as long and as crazy as you'd like
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-city"
+            >
+              City
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-city"
+              type="text"
+              placeholder="Albuquerque"
+            />
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-state"
+            >
+              State
+            </label>
+            <div className="relative">
+              <select
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-state"
+              >
+                <option>New Mexico</option>
+                <option>Missouri</option>
+                <option>Texas</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-zip"
+            >
+              Zip
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-zip"
+              type="text"
+              placeholder="90210"
+            />
+          </div>
+        </div>
+        <div className="md:w-2/3 mb-10">
+          <button
+            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
